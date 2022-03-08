ca8de75c176d9e15414f708bd0fcbf81994b72d9
Maria Meyer <mbean1216@icloud.com>
2022-03-08 15:02:29 -0600

    fixing formatting

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 // pulling elements from HTML
 var todaysDate = document.getElementById('currentDay');
+var hours = document.getElementById('hours');
+const hourColors = document.getElementsByClassName('.color');
+
 
 // adds current date to scheduler
 function date() {
-    var today = moment().format('dddd, MMMM Do');
+    var today = moment().format('dddd, MMM Do YYYY');
     todaysDate.innerHTML = today;
 }
 
 // displaying todays date
 date();
+
+// color coding past, present, and future time
+function colorCode() {
+    // setting current time
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes();
+    // linking time blocks to js
+    if (hourColors < time ) {
+        
+    } else {
+        
+    }
+}
+
+colorCode();
