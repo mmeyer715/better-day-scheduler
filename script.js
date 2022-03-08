67a01312948ff339735508f425574a563928aa13
Maria Meyer <mbean1216@icloud.com>
2022-03-08 16:11:45 -0600

    getting current time

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // pulling elements from HTML
 var todaysDate = document.getElementById('currentDay');
-var hours = document.getElementById('hours');
-const hourColors = document.getElementsByClassName('.color');
-
+var hours = document.getElementsByClassName('.time-block')
+var past = document.getElementsByClassName('.past');
 
 // adds current date to scheduler
 function date() {
@@ -17,13 +16,12 @@ date();
 function colorCode() {
     // setting current time
     var today = new Date();
-    var time = today.getHours() + ":" + today.getMinutes();
-    // linking time blocks to js
-    if (hourColors < time ) {
-        
-    } else {
-        
-    }
+    var currentTime = today.getHours() + ":" + today.getMinutes();
+
+    /* TODO
+    create function to loop through timeblock
+    create function to color code timeblocks
+    save to local storage*/
 }
 
 colorCode();
